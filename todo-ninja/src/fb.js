3e3a15690d4f478996ae432252768fb2dafed59b
jonli <jonli@tencent.com>
2019-02-18 10:28:54 +0800

    use my own fb

--- todo-ninja/src/fb.js
+++ todo-ninja/src/fb.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDyx6ZJUPA-xs_BJMkY55mb1RDvivRD0SY",
-  authDomain: "nn-veutify-files.firebaseapp.com",
-  databaseURL: "https://nn-veutify-files.firebaseio.com",
-  projectId: "nn-veutify-files",
-  storageBucket: "nn-veutify-files.appspot.com",
-  messagingSenderId: "494018042073"
+  apiKey: "AIzaSyD5YTSpRTcFOEBkrwas13k6sRLlNib0AMw",
+  authDomain: "anduo-todos.firebaseapp.com",
+  databaseURL: "https://anduo-todos.firebaseio.com",
+  projectId: "anduo-todos",
+  storageBucket: "anduo-todos.appspot.com",
+  messagingSenderId: "523721404810"
 };
 firebase.initializeApp(config);
 const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true });
+// db.settings({ timestampsInSnapshots: true });
 
 export default db;
